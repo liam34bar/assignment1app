e1543453cd1cda56802e829e7f4d7d8b5118452a
natalijana1 <natalij0403@gmail.com>
2026-01-28 18:34:03 +0200

    Add checkWinner

--- app/src/main/java/com/example/assignment1app/ui/MainActivity.kt
+++ app/src/main/java/com/example/assignment1app/ui/MainActivity.kt
@@ -42,8 +42,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkWinner(): Boolean {
-        return true
-    }
+        for (i in 0..2) {
+            if (board[i][0] != null && board[i][0] == board[i][1] && board[i][1] == board[i][2]) return true
+            if (board[0][i] != null && board[0][i] == board[1][i] && board[1][i] == board[2][i]) return true
+        }
+
+        if (board[0][0] != null && board[0][0] == board[1][1] && board[1][1] == board[2][2]) return true
+        if (board[0][2] != null && board[0][2] == board[1][1] && board[1][1] == board[2][0]) return true
+
+        return false    }
 
     private fun isBoardFull(buttons: Array<Array<Button>>): Boolean {
         return true
